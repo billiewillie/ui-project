9f561af2ac17d3d5d406e79e0c2390d34bdc2ba7
Vadim <vadimbelinovich@gmail.com>
2022-06-01 01:48:45 +0300

    added gtag

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	env: {
+		GOOGLE_ANALYTICS_ID: "G-4036XWHDX5",
+	},
 };
 
 module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,42 @@
 import Head from "next/head";
 import "../styles/globals.css";
 import Layout from "../layout/Layout.jsx";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as ga from "../lib/google-analytics";
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			ga.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
 	return (
-		<Layout>
-			<Head>
-				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
-			</Head>
-			<Component {...pageProps} />
-		</Layout>
+		<>
+			<Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} strategy='afterInteractive' />
+			<Script id='google-analytics' strategy='afterInteractive'>
+				{`
+					window.dataLayer = window.dataLayer || []; 
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date()); 
+					gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+				`}
+			</Script>
+			<Layout>
+				<Head>
+					<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				</Head>
+				<Component {...pageProps} />
+			</Layout>
+		</>
 	);
 }
 
